734250cc05ce4e223fd5760eb8e7224dc4db90cc
Nic Hampton <nicolasjhampton@gmail.com>
2019-06-16 13:58:44 -0700

    fix routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'poems/index'
+  get '/', to: 'home#index', as: 'home'
+  get '/poems', to: 'poems#index', as: 'poems'
   get 'admin/sync'
-  get 'home/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '*any_path', to: redirect('/')
 end
