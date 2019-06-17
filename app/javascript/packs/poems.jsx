bea43b3769fb629be320bd795b76f7a24f728f97
Nic Hampton <nicolasjhampton@gmail.com>
2019-06-16 21:07:52 -0700

    database data used for poems

--- app/javascript/packs/poems.jsx
+++ app/javascript/packs/poems.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
 import { Poems } from '../poems'
-import poems from '../mock_data/poems'
+// import poems from '../mock_data/poems'
 
 
 const node = document.getElementById('poems-data');
-const images = JSON.parse(node.getAttribute('data'));
+const { images, poems } = JSON.parse(node.getAttribute('data'));
 
 ReactDOM.render(
   <Poems poems={poems} images={images}/>,
