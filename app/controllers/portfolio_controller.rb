385cf6e11685523e3713646b3503ec5542d40816
Nic Hampton <nicolasjhampton@gmail.com>
2019-06-16 22:43:02 -0700

    galleries and photos based on db entries now

--- app/controllers/portfolio_controller.rb
+++ app/controllers/portfolio_controller.rb
@@ -1,5 +1,15 @@
 class PortfolioController < ApplicationController
   def index
-    render 'index', locals: { name: "Rose" }
+    render 'index', locals: { portfolio: galleries }
+  end
+
+  def galleries
+    Gallery.all.map do |gallery|
+      photos = gallery.photos.pluck(:image_url, :large_url)
+      { 
+        name: gallery.name, 
+        photos: photos.map {|photo| { source: photo[0], large: photo[1] } }
+      }
+    end
   end
 end
